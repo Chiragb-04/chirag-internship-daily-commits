2b8c4ec00fb52df159bde7607c10c2d2813fd971
Chiragb-04 <chiragb849@gmail.com>
2025-05-19 23:22:02 +0530

    add new sum functions

--- first.js
+++ first.js
@@ -1,3 +1,18 @@
 // add prompt function
 let name = prompt("Enter your name:");
 console.log("Hello, " + name + "!");
+
+ // Get input from the user
+ let num1 = prompt("Enter the first number:");
+ let num2 = prompt("Enter the second number:");
+
+// Convert to numbers (prompt returns strings)
+ num1 = Number(num1);
+ num2 = Number(num2);
+
+// Add the numbers
+let sum = num1 + num2;
+
+// Show the result
+console.log("The sum is: " + sum);
+alert("The sum is: " + sum);
